4c115527299009b57cf3c83b8f1f888ec7b5567c
pesnunj0 <javier.nunez.casanovas@gmail.com>
2017-03-09 10:22:20 +0000

    Create Folder for scheduler Project

---
+++ Geshotel/Geshotel.Web/Modules/Recepcion/Scheduler/SchedulerPage.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Geshotel.Recepcion.Pages
+{
+    using Serenity;
+    using Serenity.Web;
+    using Serenity.Data;
+    using System.Web.Mvc;
+
+    [RoutePrefix("Recepcion/Scheduler"), Route("{action=index}")]
+    //[ReadPermission("Recepcion:Hotel")]
+    //[ModifyPermission("Recepcion:Hotel")]
+    [PageAuthorize(typeof(Entities.ReservasRow))]
+    public class SchedulerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("~/Modules/Recepcion/Scheduler/SchedulerIndex.cshtml");
+        }
+    }
+}
